6cc3014f86c682ef4730681c9c08e3e870554ea8
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-08-07 21:30:53 +0530

    It changing

--- admin/src/pages/Edit.jsx
+++ admin/src/pages/Edit.jsx
@@ -222,7 +222,7 @@ const Edit = ({ token }) => {
       <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
 
         <div>
-          <p className='mb-2'>Product category  ....vdv</p>
+          <p className='mb-2'>Product category</p>
           <select onChange={(e) => setCategory(e.target.value)} value={category} className='w-full px-3 py-2'>
             <option value="Men">Men</option>
             <option value="Women">Women</option>
